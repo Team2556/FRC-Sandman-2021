d80a89d92dee74d319427a4bfa4949b883904f71
Matthew VandenBoom <Bookster99@outlook.com>
2021-01-25 12:11:13 -0600

    create the classes

---
+++ src/main/include/RobotMap.h
@@ -0,0 +1,10 @@
+// -----------------------------------
+// CAN Bus Devices
+// -----------------------------------
+
+// Motor Controllers
+
+#define DRIVE_LEFT_FRONT 1
+#define DRIVE_RIGHT_FRONT 2
+#define DRIVE_LEFT_REAR 3
+#define DRIVE_RIGHT_REAR 4
