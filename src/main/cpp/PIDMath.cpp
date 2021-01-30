25115d6fd20d3e34999c11c9df056bd7d0104eff
somnomly <brnzbrwn@gmail.com>
2021-01-30 12:43:45 -0600

    Started work on PIDMath, still broken. Still need to define a bunch of variables such as YawPID etc.

--- src/main/cpp/PIDMath.cpp
+++ src/main/cpp/PIDMath.cpp
@@ -3,5 +3,33 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #include "PIDMath.h"
+#include "math.h"
 
-PIDMath::PIDMath() = default;
+PIDMath::PIDMath(){
+    pPIDOutput = new TurretPIDOutput(&fYawPIDValue);
+    pYawPID    = new frc::PIDController(-.031, 0.0, 0.0, 0.0, this, pPIDOutput);
+
+};
+
+class TurretPIDOutput : public frc::PIDOutput
+{
+	public:
+		TurretPIDOutput(float * pfYawPIDVar) : pfYawPID(pfYawPIDVar) {}
+
+		float       * pfYawPID;
+
+		void PIDWrite(double dOutput) { *pfYawPID = dOutput; }
+};
+
+void Shooter::Aim()
+{
+		float value = fYawPIDValue;
+		pRobot->Turret_Motor.Set(value);
+		ShooterDebug.PutNumber("PID Output",value);
+};
+
+ TurretPIDOutput     * pPIDOutput;
+	frc::PIDController  * pYawPID;
+  float fYawPIDValue;
+  void    PIDEnable(bool bEnable);
+  double  PIDGet();
